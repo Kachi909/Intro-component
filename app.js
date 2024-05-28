846e1f068457857d33b55a791852bdd42e382160
chikachi ukpanwanne <chikachi909@gmail.com>
2024-05-28 21:04:33 +0100

    intro-component-with signup-1

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const firstName = document.querySelector('.firstName');
 const lastName = document.querySelector('.lastName');
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
+const togglePassword = document.getElementById('togglePassword');
+
+const errorIcon = document.querySelectorAll('.error_icon');
+const errorText = document.querySelectorAll('.error_text');
+const emailSample = document.querySelectorAll('.email_sample');
+
 
 console.log(firstName);
 
@@ -17,33 +23,53 @@ form.addEventListener('submit', (e) => {
   const passwordVal = password.value;
   console.log(fName, lName, emailVal, passwordVal);
 
+
+
   // Check first name
   if (fName === '') {
     firstName.classList.add('error');
+    errorIcon[0].classList.remove('hide')
+    errorText[0].classList.remove('hide')
   } else {
     firstName.classList.remove('error');
+    errorIcon[0].classList.add('hide')
+    errorText[0].classList.add('hide')
   }
   // Check last name
 
   if (lName === '') {
     lastName.classList.add('error');
+    errorIcon[1].classList.remove('hide')
+    errorText[1].classList.remove('hide')
   } else {
     lastName.classList.remove('error');
+    errorIcon[1].classList.add('hide')
+    errorText[1].classList.add('hide')
   }
   // Check email
 
   if (!validateEmail(emailVal) || emailVal === '') {
     email.classList.add('error');
+    errorIcon[2].classList.remove('hide')
+    errorText[2].classList.remove('hide')
+    emailSample[0].classList.remove('hide')
   } else {
     email.classList.remove('error');
+    errorIcon[2].classList.add('hide')
+    errorText[2].classList.add('hide')
+    emailSample[0].classList.add('hide')
   }
 
   // Check password
 
   if (passwordVal === '') {
     password.classList.add('error');
+    errorIcon[3].classList.remove('hide')
+    errorText[3].classList.remove('hide')
   } else {
     password.classList.remove('error');
+    errorIcon[3].classList.add('hide')
+    errorText[3].classList.add('hide')
   }
 });
 
@@ -53,3 +79,10 @@ function validateEmail(email) {
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+
+togglePassword.addEventListener('click', function () {
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  this.classList.toggle('fa-eye');
+  this.classList.toggle('fa-eye-slash');
+});
